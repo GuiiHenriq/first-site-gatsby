14aab872f7e43feb60421a2521c9cd9b632ce576
Guilherme Erba <guilhermeerba@hotmail.com>
2020-05-22 16:12:59 -0300

    first commit

---
+++ src/components/Profile/index.js
@@ -0,0 +1,53 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+
+const Profile = () => { // USE STATIC QUERY - HOOK DO REACT
+  const {
+    site: { 
+      siteMetadata : { title, position, description }
+    }
+  } = useStaticQuery(graphql`
+      query MySiteMetadata {
+        site {
+          siteMetadata {
+            title
+            position
+            description
+          }
+        }
+      }
+    `)
+
+  return (
+    <div className="Profile-react">
+      <h1>{title}</h1>
+      <h2>{position}</h2>
+      <p>{description}</p>
+    </div>
+  )
+}
+
+/*const Profile = () => ( // STATIC QUERY - COMPONENTE DO REACT
+  <StaticQuery
+    query={graphql`
+      query MySiteMetadata {
+        site {
+          siteMetadata {
+            title
+            position
+            description
+          }
+        }
+      }
+    `}
+    render={({site: { siteMetadata : { title, position, description }}}) => (
+      <div className="Profile-react">
+        <h1>{title}</h1>
+        <h2>{position}</h2>
+        <p>{description}</p>
+      </div>
+    )}
+  />
+)*/
+
+export default Profile
